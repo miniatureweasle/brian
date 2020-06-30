527cc0e45d9ae253267d8a6f42e795a0ff21b4bf
Jacob Goldberg <jg123jg123@gmail.com>
2020-07-01 00:58:51 +0530

    Add the ability to render multiple stars

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,36 +5,37 @@ import "./App.css";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {}
+ }
+
+
+  componentDidMount(){
+    const canvas = this.refs.starryNight
+    this.setState({ctx: canvas.getContext("2d")})
   }
 
-  componentDidMount() {
-    const canvas = this.refs.starryNight;
-    this.setState({ ctx: canvas.getContext("2d") });
+  renderStar(ctx, x,y){
+    ctx.fillStyle = "white"
+    ctx.fillRect(x,y, 6, 6);
   }
 
-  renderStar(ctx, x, y) {
-    ctx.fillStyle = "white";
-    ctx.fillRect(x, y, 6, 6);
-  }
-
-  renderStars(ctx, number) {
-    for (let i = 0; i < number; i++) {
-      let x = Math.random() * 1000;
-      let y = Math.random() * 1000;
-      this.renderStar(ctx, x, y);
-    }
+  renderStars(ctx, number){
+      for (let i=0; i<number;i++){
+          let x = Math.random() * 1000
+          let y = Math.random() * 1000
+          this.renderStar(ctx, x, y)
+      }
   }
 
   render() {
-    if (this.state.ctx) {
-      this.renderStars(this.state.ctx, 12);
+    if (this.state.ctx){
+       this.renderStars(this.state.ctx, 12)
     }
     return (
       <div className="App">
         <div>Starry night</div>
         <div>
-          <canvas ref="starryNight" width={300} height={300}></canvas>
+          <canvas ref="starryNight" width={300} height={300} ></canvas>
         </div>
       </div>
     );
