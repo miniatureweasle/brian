134aea7eecdc450017d5432ba5f378195d656d05
Jacob Goldberg <jg123jg123@gmail.com>
2020-07-01 00:26:02 +0530

    Refactor App (prettier)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,78 +37,24 @@ class App extends Component {
       // starGlow.addColorStop(1, "gold");
       
       
-      
-      function star(ctx, x, y, r, p, m)
-      {
-          ctx.save();
-          ctx.beginPath();
-          ctx.translate(x, y);
-          ctx.moveTo(0,0-r);
-          for (var i = 0; i < p; i++)
-          {
-              ctx.rotate(Math.PI / p);
-              ctx.lineTo(0, 0 - (r*m));
-              ctx.rotate(Math.PI / p);
-              ctx.lineTo(0, 0 - r);
-          }
-          ctx.fill();
-          ctx.restore();
-      }
-      
-      function box(input, parameter){
-          reducedParameter = parameter -100;
-          if(input <100){
-              input +=100;
-          }else if(input >reducedParameter){
-              input -=100
-          }
-          let output = input;
-          return output;
-      }
-      
-      //generate stars
-      while(starQuantity != 0){
-      
-          //let random = Math.random();
-      
-          let xrandom = Math.floor(width*Math.random());
-          let yrandom = Math.floor(height*Math.random());
-      
-          console.log(box(xrandom, width));
-          console.log(box(yrandom, height));
-      
-          let boxValueY = box(yrandom, height);
-          let boxValueX = box(xrandom, width);
-      
-          starQuantity --;
-          //ctx.fillRect(xrandom,yrandom,9,9);
-          
-          ctx.beginPath();
-         // ctx.arc(yrandom, xrandom, 3, 0, 2 * Math.PI, false);
-         star(ctx,boxValueX, boxValueY,9,5,0.5)
-          ctx.fillStyle = "white";
-          ctx.strokeStyle = starGlow;
-          ctx.fill();
-          ctx.stroke()
-      }
-      
+     
       //ctx.fillRect(2000,100,3,3);
       
 
 
-  return(
-  <div className="App">
-      
-  <div>Starry night</div>
-  <div>
-  <canvas id="starryNight">
+    return(
+    <div className="App">
+        
+    <div>Starry night</div>
+    <div>
+    <canvas id="starryNight">
 
-  </canvas>
+    </canvas>
 
-  </div>
+    </div>
 
-  </div>
-  )
+    </div>
+    )
     
 }
 }
