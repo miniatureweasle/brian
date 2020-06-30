6bfaa977ecad4afe9bf1075cb7a7a3fe1099e364
Jacob Goldberg <jg123jg123@gmail.com>
2020-07-01 00:58:13 +0530

    Add basic starry night app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,63 +1,44 @@
 import React, { Component } from "react";
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-  constructor(props){
-      super(props)
-   }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  componentDidMount() {
+    const canvas = this.refs.starryNight;
+    this.setState({ ctx: canvas.getContext("2d") });
+  }
 
+  renderStar(ctx, x, y) {
+    ctx.fillStyle = "white";
+    ctx.fillRect(x, y, 6, 6);
+  }
 
-   
+  renderStars(ctx, number) {
+    for (let i = 0; i < number; i++) {
+      let x = Math.random() * 1000;
+      let y = Math.random() * 1000;
+      this.renderStar(ctx, x, y);
+    }
+  }
 
-
-
-    render(){
-
-      const goldenRatio = 1.618;
-      let width = 1200;
-      let height = width/goldenRatio
-      
-      var starQuantity = 10;
-      
-      //defining canvas 
-      const canvas =  document.getElementById("starryNight");
-      canvas.style.backgroundColor = "black";
-      const ctx = canvas.getContext("2d");
-      
-      //window.innerWidth/Height sets the canvas element to full the intire size of the page.
-      
-      canvas.width = width;
-      canvas.height = height;
-      
-      //styling stars
-      var starGlow = ctx.createRadialGradient(75, 50, 5, 90, 60, 100);
-      starGlow.addColorStop(0, "white");
-      // starGlow.addColorStop(1, "gold");
-      
-      
-     
-      //ctx.fillRect(2000,100,3,3);
-      
-
-
-    return(
-    <div className="App">
-        
-    <div>Starry night</div>
-    <div>
-    <canvas id="starryNight">
-
-    </canvas>
-
-    </div>
-
-    </div>
-    )
-    
+  render() {
+    if (this.state.ctx) {
+      this.renderStars(this.state.ctx, 12);
+    }
+    return (
+      <div className="App">
+        <div>Starry night</div>
+        <div>
+          <canvas ref="starryNight" width={300} height={300}></canvas>
+        </div>
+      </div>
+    );
+  }
 }
-}
-
 
 export default App;
